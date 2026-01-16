47a1f025555627e45d13dad1cbb8881f7065d4da
Talat Gedikli <atalatgedikli@gmail.com>
2026-01-16 23:23:22 +0300

    Product ve Category Modeli Eklendi

---
+++ src/models/Product.js
@@ -0,0 +1,30 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const Product = sequelize.define('Product', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    description: {
+        type: DataTypes.TEXT,
+    },
+    price: {
+        type: DataTypes.DECIMAL(10, 2),
+        allowNull: false,
+    },
+    stock: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0,
+    },
+    // Foreign Key will be added in associations
+}, {
+    timestamps: true,
+});
+
+module.exports = Product;
